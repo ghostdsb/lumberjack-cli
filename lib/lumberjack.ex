f2ef461d9f219d9071af3c424d383ce4cab16067
dibyanshu <dibyanshu@gamezop.co>
2021-05-11 18:18:59 +0530

    endstate board

--- lib/lumberjack.ex
+++ lib/lumberjack.ex
@@ -40,13 +40,51 @@ defmodule Lumberjack do
     end
     )
   end
-  # def run do
-  #   read_file()
-  #   |> String.split("\n")
-  #   |> Enum.filter(fn line -> line |> is_result end)
-  #   |> Enum.map(fn line -> line |> get_battle_ids end)
-  #   |> IO.inspect()
-  # end
+
+  def battle_status(file, battle_id, complaint) do
+    battle_log_list =
+      file
+      |> read_file()
+      |> String.split("\n")
+      |> Enum.filter(fn line -> line |> is_t3 end)
+      |> Enum.filter(fn line -> line |> battle_logs(battle_id) end)
+
+    game_state =
+      battle_log_list
+      |> Enum.filter(fn log -> not is_result(log) end)
+      |> Enum.map(fn log -> log |> get_player_id_move(complaint) end)
+      |> Map.new()
+
+    game_state
+    |> draw_board()
+
+    result =
+      battle_log_list
+      |> Enum.filter(fn log -> log |> is_result end)
+      result
+  end
+
+  def draw_board(game_state) do
+     game_state
+    board = 0..8
+    |> Enum.map(fn pos -> game_state[pos |> to_string] || " - " end )
+    |> Enum.chunk_every(3)
+    |> Enum.reduce("\n|---|---|---|\n" , fn line_list,acc -> acc<>Enum.reduce(line_list, "", fn id,acc -> acc<>"|"<>id end)<>"|\n|---|---|---|\n" end)
+
+    board
+    |> IO.puts()
+  end
+
+  def get_player_id_move(log_string, complaint) do
+    [_ts, _battle_id_info, _info, player_id, _arrow, move] =
+      log_string
+      |> String.split(" ")
+    cond do
+      complaint === player_id -> {move, " X "}
+      true -> {move, " O "}
+    end
+
+  end
 
   def read_file(file) do
     with {:ok, content} <- File.read(file) do
@@ -77,6 +115,11 @@ defmodule Lumberjack do
       line_string
       |> String.split(" ")
     reason
+  end
+
+  def battle_logs(log_lines, battle_id) do
+    log_lines
+    |> String.contains?(battle_id)
   end
 
   def get_battle_ids(line_string) do
